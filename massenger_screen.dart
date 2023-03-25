0f5a6063937f917c1623c6138205e7ccbaeb08ee
Anas A Taleeb <102483320+AnasATaleeb@users.noreply.github.com>
2023-03-25 17:52:24 +0300

    Add files via upload

---
+++ massenger_screen.dart
@@ -0,0 +1,252 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class MessengerScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        titleSpacing: 20,
+        title: Row(
+          children: [
+            CircleAvatar(
+              radius: 20.0,
+              backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/102483320?s=400&u=126c5a73de82a7414f0d2bcba98e56c245dda473&v=4'),
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Text(
+              'Chats',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: CircleAvatar(
+              radius: 15.0,
+              backgroundColor: Colors.blue,
+              child: Icon(
+                Icons.camera_alt,
+                size: 16,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: CircleAvatar(
+              radius: 15.0,
+              backgroundColor: Colors.blue,
+              child: Icon(
+                Icons.edit,
+                size: 16,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      //body
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment:  CrossAxisAlignment.start,
+            children:
+            [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.grey[300],
+                ),
+                padding: EdgeInsets.all(5),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      'Search',
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 100,
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context,index){
+                      return buildStoryItem();
+                  },
+                  itemCount: 5, separatorBuilder: (context, index)=>SizedBox(width: 20.0,) ,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ListView.separated(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemBuilder: (context,index) => buildChatItem(),
+                  separatorBuilder: (context,index) => SizedBox(height: 20,),
+                  itemCount: 15,
+              ),
+              
+
+            ],
+          ),
+        ),
+
+      ),
+    );
+  }
+
+
+  Widget buildChatItem() =>Row(
+    children: [
+      Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          CircleAvatar(
+            radius: 30.0,
+            backgroundImage: NetworkImage(
+                'https://avatars.githubusercontent.com/u/102483320?s=400&u=126c5a73de82a7414f0d2bcba98e56c245dda473&v=4'),
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.only(
+              bottom: 3,
+              end: 3,
+            ),
+            child: CircleAvatar(
+              radius: 8.0,
+              backgroundColor: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.only(
+              bottom: 3,
+              end: 3,
+            ),
+            child: CircleAvatar(
+              radius: 7.0,
+              backgroundColor: Colors.green,
+            ),
+          ),
+        ],
+      ),
+      SizedBox(
+        width: 20.0,
+      ),
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Anas Ayman Taleeb',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Hello my name is anas ayman taleeb i am a computer scince student in BZU ',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Container(
+                    width: 7,
+                    height: 7,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+
+                Text('02:00 PM'),
+              ],
+            ),
+
+
+          ],
+        ),
+      ),
+    ],
+  );
+
+  Widget buildStoryItem() =>Container(
+    width: 60,
+    child: Column(
+      children: [
+        Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            CircleAvatar(
+              radius: 30.0,
+              backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/102483320?s=400&u=126c5a73de82a7414f0d2bcba98e56c245dda473&v=4'),
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.only(
+                bottom: 3,
+                end: 3,
+              ),
+              child: CircleAvatar(
+                radius: 8.0,
+                backgroundColor: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsetsDirectional.only(
+                bottom: 3,
+                end: 3,
+              ),
+              child: CircleAvatar(
+                radius: 7.0,
+                backgroundColor: Colors.green,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 6.0,
+        ),
+        Text(
+          'Anas Ayman Taleeb',
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+
+        ),
+      ],
+    ),
+  );
+}
